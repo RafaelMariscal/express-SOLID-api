35019b8f1b68630a95510e378aee022c5c59199b
Rafael Mariscal <rafael_mariscal_@outlook.com>
2022-08-25 09:21:33 -0300

    Add/List categories route

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,23 +1,19 @@
 import { Router } from "express";
 
-import { Category } from "../model/Category";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
-
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (req, res) => {
   const { name, description } = req.body;
-  const category = new Category();
-  Object.assign(category, {
-    name,
-    description,
-    created_At: new Date(),
-  });
 
-  categories.push(category);
+  categoriesRepository.create({ name, description });
 
-  return res.status(201).send(categories);
+  return res.status(201).send();
 });
-
+categoriesRoutes.get("/", (req, res) => {
+  const all = categoriesRepository.list();
+  return res.json(all);
+});
 export { categoriesRoutes };
