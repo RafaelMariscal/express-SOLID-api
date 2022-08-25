9f5e2154f5d9b355365fda6cba0f94b09bfd3880
Rafael Mariscal <rafael_mariscal_@outlook.com>
2022-08-25 08:56:33 -0300

    Add/Create category route

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,13 @@
-import express, { json } from "express";
+import express from "express";
+
+import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
 const port = 3333;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({ message: "Hello World!" });
-});
-
-app.post("/courses", (req, res) => {
-  const { name } = req.body;
-  return res.json({ name });
-});
+app.use("/categories", categoriesRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
