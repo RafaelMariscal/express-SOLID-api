e9df7ed1d80f2bfd1f747ab8585c43b138892e69
Rafael Mariscal <rafael_mariscal_@outlook.com>
2022-08-25 19:26:07 -0300

    Add/Validation to create category route

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -7,13 +7,20 @@ const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (req, res) => {
   const { name, description } = req.body;
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExists) {
+    return res.status(400).json({ error: "category already exists" });
+  }
 
   categoriesRepository.create({ name, description });
 
   return res.status(201).send();
 });
+
 categoriesRoutes.get("/", (req, res) => {
   const all = categoriesRepository.list();
   return res.json(all);
 });
+
 export { categoriesRoutes };
