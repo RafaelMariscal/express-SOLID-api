e8ab24092e545cfd5f59d84a852410b4be51c2fc
Rafael Mariscal <rafael_mariscal_@outlook.com>
2022-08-25 08:22:43 -0300

    Add/Debugger settings

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
-import express from "express";
+import express, { json } from "express";
 
 const app = express();
 const port = 3333;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.json({ message: "Hello World!" });
+});
+
+app.post("/courses", (req, res) => {
+  const { name } = req.body;
+  return res.json({ name });
 });
 
 app.listen(port, () => {
